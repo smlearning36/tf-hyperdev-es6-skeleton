f1194d3a07252f144ebc800018711cf9c81290a7
Benjamin E White <benjamin.e.white1@gmail.com>
2016-09-29 16:47:31 -0400

    zero out server.js

--- server.js
+++ server.js
@@ -3,24 +3,11 @@
 const express = require('express');
 const app = express();
 
-// note that in memory variables are NOT normally
-// how to store state on the server, but we're simplifying
-// here to get a sense of express basics.
-let theCount = 0;
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => res.sendFile(__dirname + 'index.html'));
-
-
-app.get("/the-count", (req, res) => {
-  console.log('incrementing the count');
-  theCount += 1;
-  console.log(`the count is ${theCount}`);
-  res.json({count: theCount});
-});
 
 // listen for requests :)
 app.listen(process.env.PORT, () => console.log(
