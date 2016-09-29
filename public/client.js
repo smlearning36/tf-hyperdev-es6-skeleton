e3a30560c148074b1280f0c7cc0f782acb5f9ff9
Benjamin E White <benjamin.e.white1@gmail.com>
2016-09-29 16:54:19 -0400

    zero out client.js

--- public/client.js
+++ public/client.js
@@ -1,15 +1,2 @@
 // client-side js
-function getCount() {
-  $.get('/the-count', function(data) {
-    $('.js-current-count').text(data.count);
-  });
-}
 
-$(function() {
-  getCount();
-  $('form').submit(function(event) {
-    event.preventDefault();
-    getCount();
-  });
-
-});
