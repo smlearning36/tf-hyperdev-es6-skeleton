e6033efa3530806f5378420526a5eb79ee0f6525
Benjamin E White <benjamin.e.white1@gmail.com>
2016-09-29 11:16:54 -0400

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-var express = require('express');
-var app = express();
+'use strict';
+
+const express = require('express');
+const app = express();
 
 // note that in memory variables are NOT normally
 // how to store state on the server, but we're simplifying
 // here to get a sense of express basics.
-var theCount = 0;
+let theCount = 0;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-   res.sendFile(__dirname + 'index.html');
-});
+app.get("/", (req, res) => res.sendFile(__dirname + 'index.html'));
 
-app.get("/the-count", function (req, res) {
+
+app.get("/the-count", (req, res) => {
   console.log('incrementing the count');
   theCount += 1;
+  console.log(`the count is ${theCount}`);
   res.json({count: theCount});
 });
 
 // listen for requests :)
-app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + this.address().port);
-});
+app.listen(process.env.PORT, () => console.log(
+  `Your app is listening on port ${process.env.PORT}`));
